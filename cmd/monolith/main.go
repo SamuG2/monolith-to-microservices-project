6816306a9616ecc18588ecfd16e446c5b4d673d3
unknown <samgarciag@gmail.com>
2022-06-19 20:11:54 +0200

    Creada estructura del proyecto.

---
+++ cmd/monolith/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "log"
+
+func createMonolith() {
+
+}
+
+func main() {
+	log.Println("Arrancando versión monolítica...")
+	createMonolith()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SamuG2/monolith-to-microservices-golang
+
+go 1.18
